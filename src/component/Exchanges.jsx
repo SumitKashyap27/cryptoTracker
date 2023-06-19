22071a65bf25d1f69b9cd1da5b1433e8ce0b570b
SumitKashyap27 <sumit20020927@gmail.com>
2023-06-19 13:03:21 +0530

    exchanges sectiion updated

--- src/component/Exchanges.jsx
+++ src/component/Exchanges.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import axios from 'axios'
-
+import React, { useEffect, useState } from 'react'
+import axios from "axios";
+import { server } from '..';
+import { Container } from '@chakra-ui/react';
 const Exchanges = () => {
-  return (
-    <div>Exchanges</div>
-  )
+
+const [exchanges,setExchanges] = useState([]);
+
+  useEffect(()=>{
+    const fetchExchanges = async()=>{
+      const {data} = await axios.get(`${server}/exchanges`);
+      setExchanges(data);
+    };
+    fetchExchanges();
+  }, []);
+  return <Container maxW={"container.xl"}>da
+
+  </Container>
 }
 
 export default Exchanges
