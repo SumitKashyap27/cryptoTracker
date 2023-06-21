980a800fa3b073c3265a37d939a0480a9aad1d0b
SumitKashyap27 <sumit20020927@gmail.com>
2023-06-21 23:56:25 +0530

    initial commir

--- src/component/Exchanges.jsx
+++ src/component/Exchanges.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from "axios";
 import { server } from '..';
-import { Container, HStack, VStack, Image } from '@chakra-ui/react';
+import { Container, HStack, VStack, Image, Heading, Text } from '@chakra-ui/react';
 import Loader  from './Loader';
 
 const Exchanges = () => {
@@ -9,12 +9,12 @@ const Exchanges = () => {
 const [exchanges,setExchanges] = useState([]);
 const [loading,setLoading] = useState(true);
 
+
   useEffect(()=>{
     const fetchExchanges = async()=>{
       const {data} = await axios.get(`${server}/exchanges`);
       setExchanges(data);
       setLoading(false);
-      console.log(data);
     };
     fetchExchanges();
   }, []);
@@ -32,7 +32,19 @@ const [loading,setLoading] = useState(true);
 };
 const ExchangeCard = ({name,img,rank,url})=>
 <a href={url} target={"blank"}>
-  <VStack>
+  <VStack
+  w={"52"}
+  shadow={"lg"}
+  p={"8"}
+  borderRadius={"lg"}
+  transition={"all 0.3s"}
+  m={"4"}
+  css={{
+    "&:hover":{
+      transform: "scale(1.1)",
+    },
+  }}
+  >
     <Image
       src={img}
       w={"10"}
@@ -40,6 +52,10 @@ const ExchangeCard = ({name,img,rank,url})=>
       objectFit={"contain"}
       alt={"Exchange"}
     />
+    <Heading size={"md"} noOfLines={1}>
+      {rank}
+    </Heading>
+    <Text noOfLines={1}>{name}</Text>
   </VStack>
 
   </a>
