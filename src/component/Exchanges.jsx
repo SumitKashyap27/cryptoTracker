ca8dd1989d38ffd5a760238a813d5290a6a6e0b0
SumitKashyap27 <sumit20020927@gmail.com>
2023-06-22 00:16:07 +0530

    initial commit

--- src/component/Exchanges.jsx
+++ src/component/Exchanges.jsx
@@ -3,21 +3,35 @@ import axios from "axios";
 import { server } from '..';
 import { Container, HStack, VStack, Image, Heading, Text } from '@chakra-ui/react';
 import Loader  from './Loader';
+import ErrorComponent from './ErrorComponent';
 
 const Exchanges = () => {
 
 const [exchanges,setExchanges] = useState([]);
 const [loading,setLoading] = useState(true);
+const [error, setError] = useState(false);
 
 
   useEffect(()=>{
     const fetchExchanges = async()=>{
-      const {data} = await axios.get(`${server}/exchanges`);
-      setExchanges(data);
-      setLoading(false);
+      try{
+        const {data} = await axios.get(`${server}/exchanges`);
+        setExchanges(data);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+
     };
     fetchExchanges();
   }, []);
+
+  if(error) 
+  return <ErrorComponent message={"Error while fetching Ecxhanges"} />;
+
+
+
   return (
   <Container maxW={"container.xl"}>{loading? <Loader /> : 
   <>
