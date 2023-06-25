36b6c6860bcb888eb3812609b62b771dfca20c0f
SumitKashyap27 <sumit20020927@gmail.com>
2023-06-25 23:56:32 +0530

    small updates in coin details

--- src/component/CoinDetails.jsx
+++ src/component/CoinDetails.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react'
-import { Box, Container } from '@chakra-ui/react';
+import { Box, Container, HStack, Radio, RadioGroup, VStack, Text } from '@chakra-ui/react';
 import Loader from './Loader';
 import { useParams } from 'react-router-dom';
 import { server } from '../index';
 import axios from 'axios';
+import ErrorComponent from './ErrorComponent';
 
 const CoinDetails = () => {
 
-
+  const params = useParams()
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
@@ -16,9 +17,7 @@ const CoinDetails = () => {
   useEffect(() => {
     const fetchCoin = async () => {
       try {
-        const { data } = await axios.get(
-          `${server}/coins/markets?vs_currency=${currency}&page=${page}`
-        );
+        const { data } = await axios.get(`${server}/coins/${params.id}`);
         setCoins(data);
         setLoading(false);
       } catch (error) {
@@ -27,7 +26,9 @@ const CoinDetails = () => {
       }
     };
     fetchCoin();
-  }, []);
+  }, [params.id]);
+
+  if (error) return <ErrorComponent message={"Error While Fetching Coins"} />;
 
 
   return <Container maxW={"container.xl"}>
@@ -36,13 +37,30 @@ const CoinDetails = () => {
       <Loader />
       ) : (
         <>
-        <Box width={""} borderWidth={1}>
+        <Box width={"full"} borderWidth={1}>
           kjvhv
         </Box>
+
+
+        <RadioGroup value={currency} onChange={setCurrency} p={"8"}>
+            <HStack spacing={"4"}>
+              <Radio value={"inr"}>INR</Radio>
+              <Radio value={"usd"}>USD</Radio>
+              <Radio value={"eur"}>EUR</Radio>
+            </HStack>
+          </RadioGroup>
+
+          <VStack spacing={"4"} p={"16"} alignItems={"flex-start"}>
+            <Text fontSize={"small"} alignSelf={"center"} opacity={0.7}>
+              Last Updated on {Date()}
+
+            </Text>
+
+          </VStack>
         </>
       )
     }
-  </Container>
+  </Container> 
 }
 
 export default CoinDetails;
--- src/component/Exchanges.jsx
+++ src/component/Exchanges.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from "axios";
-import { server } from '..';
+import { server } from '../index';
 import { Container, HStack, VStack, Image, Heading, Text } from '@chakra-ui/react';
 import Loader  from './Loader';
 import ErrorComponent from './ErrorComponent';
