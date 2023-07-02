297343974d8dfcd59969efa4d64f33a38c312ce1
SumitKashyap27 <sumit20020927@gmail.com>
2023-07-02 23:23:02 +0530

    chart complete

--- src/component/CoinDetails.jsx
+++ src/component/CoinDetails.jsx
@@ -1,26 +1,95 @@
-import React, { useEffect, useState } from 'react'
-import { Box, Container, HStack, Radio, RadioGroup, VStack, Text, Image, Stat, StatLabel, StatNumber, StatArrow, StatHelpText, Badge, Progress } from '@chakra-ui/react';
-import Loader from './Loader';
-import { useParams } from 'react-router-dom';
-import { server } from '../index';
-import axios from 'axios';
-import ErrorComponent from './ErrorComponent';
+import {
+  Badge,
+  Box,
+  Button,
+  Container,
+  HStack,
+  Image,
+  Progress,
+  Radio,
+  RadioGroup,
+  Stat,
+  StatArrow,
+  StatHelpText,
+  StatLabel,
+  StatNumber,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { server } from "../index";
+import Chart from "./Chart";
+import ErrorComponent from "./ErrorComponent";
+import Loader from "./Loader";
 
 const CoinDetails = () => {
-
-  const params = useParams()
-  const [coins, setCoins] = useState([]);
+  const params = useParams();
+  const [coin, setCoin] = useState({});
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [currency, setCurrency] = useState("inr");
+  const [days, setDays] = useState("24h");
+  const [chartArray, setChartArray] = useState([]);
+
   const currencySymbol =
     currency === "inr" ? "₹" : currency === "eur" ? "€" : "$";
+
+  const btns = ["24h", "7d", "14d", "30d", "60d", "200d", "1y", "max"];
+
+  const switchChartStats = (key) => {
+    switch (key) {
+      case "24h":
+        setDays("24h");
+        setLoading(true);
+        break;
+      case "7d":
+        setDays("7d");
+        setLoading(true);
+        break;
+      case "14d":
+        setDays("14d");
+        setLoading(true);
+        break;
+      case "30d":
+        setDays("30d");
+        setLoading(true);
+        break;
+      case "60d":
+        setDays("60d");
+        setLoading(true);
+        break;
+      case "200d":
+        setDays("200d");
+        setLoading(true);
+        break;
+      case "1y":
+        setDays("365d");
+        setLoading(true);
+        break;
+      case "max":
+        setDays("max");
+        setLoading(true);
+        break;
+
+      default:
+        setDays("24h");
+        setLoading(true);
+        break;
+    }
+  };
 
   useEffect(() => {
     const fetchCoin = async () => {
       try {
         const { data } = await axios.get(`${server}/coins/${params.id}`);
-        setCoins(data);
+
+        const { data: chartData } = await axios.get(
+          `${server}/coins/${params.id}/market_chart?vs_currency=${currency}&days=${days}`
+        );
+        setCoin(data);
+        setChartArray(chartData.prices);
         setLoading(false);
       } catch (error) {
         setError(true);
@@ -28,22 +97,33 @@ const CoinDetails = () => {
       }
     };
     fetchCoin();
-  }, [params.id]);
+  }, [params.id, currency, days]);
 
-  if (error) return <ErrorComponent message={"Error While Fetching Coins"} />;
+  if (error) return <ErrorComponent message={"Error While Fetching Coin"} />;
 
-
-  return <Container maxW={"container.xl"}>
-    {
-      loading ? (
-      <Loader />
+  return (
+    <Container maxW={"container.xl"}>
+      {loading ? (
+        <Loader />
       ) : (
         <>
-        <Box width={"full"} borderWidth={1}>
-        </Box>
+          <Box width={"full"} borderWidth={1}>
+            <Chart arr={chartArray} currency={currencySymbol} days={days} />
+          </Box>
 
+          <HStack p="4" overflowX={"auto"}>
+            {btns.map((i) => (
+              <Button
+                disabled={days === i}
+                key={i}
+                onClick={() => switchChartStats(i)}
+              >
+                {i}
+              </Button>
+            ))}
+          </HStack>
 
-        <RadioGroup value={currency} onChange={setCurrency} p={"8"}>
+          <RadioGroup value={currency} onChange={setCurrency} p={"8"}>
             <HStack spacing={"4"}>
               <Radio value={"inr"}>INR</Radio>
               <Radio value={"usd"}>USD</Radio>
@@ -51,50 +131,72 @@ const CoinDetails = () => {
             </HStack>
           </RadioGroup>
 
-          <VStack spacing={"4"} p={"16"} alignItems={"flex-start"}>
-            <Text fontSize={"small"} alignSelf={"center"} opacity={0.7}>
-              Last Updated on {Date().split("G")[0] }
+          <VStack spacing={"4"} p="16" alignItems={"flex-start"}>
+            <Text fontSize={"small"} alignSelf="center" opacity={0.7}>
+              Last Updated On{" "}
+              {Date(coin.market_data.last_updated).split("G")[0]}
             </Text>
-            <Image 
-            src={coins.image.large}
-            w={"16"}
-            h={"16"}
-            objectFit={"contain"}
+
+            <Image
+              src={coin.image.large}
+              w={"16"}
+              h={"16"}
+              objectFit={"contain"}
             />
+
             <Stat>
-              <StatLabel>{coins.name}</StatLabel>
+              <StatLabel>{coin.name}</StatLabel>
               <StatNumber>
                 {currencySymbol}
-                {coins.market_data.current_price[currency]}</StatNumber>
+                {coin.market_data.current_price[currency]}
+              </StatNumber>
               <StatHelpText>
-              <StatArrow type={coins.market_data.price_change_percentage_24h > 0
-                ?"increase":
-                "decrease"
-                }
+                <StatArrow
+                  type={
+                    coin.market_data.price_change_percentage_24h > 0
+                      ? "increase"
+                      : "decrease"
+                  }
                 />
-              {coins.market_data.price_change_percentage_24h}%
+                {coin.market_data.price_change_percentage_24h}%
               </StatHelpText>
             </Stat>
-<Badge fontSize={"2xl"}
-    bgColor={"blackAlpha.800"}
-    color={"white"}>
-    {`#${coins.market_cap_rank}`}
-</Badge>
-<CustomBar 
-high={`${currencySymbol}${coins.market_data.high_24h[currency]}`} 
-low={`${currencySymbol}${coins.market_data.low_24h[currency]}`}/>
-        <Box>
-          <Item title = {"Max Supply"} value={coins.market_data.max_supply}/>
-          <Item title = {"Circulating Supply"} value={coins.market_data.circulating_supply}/>
-          <Item title = {"Market Capital"} value={`${currencySymbol}${coins.market_data.market_cap[currency]}`}/>
-          <Item title = {"All time Low"} value={`${currencySymbol}${coins.market_data.atl[currency]}`}/>
-          <Item title = {"All time High"} value={`${currencySymbol}${coins.market_data.ath[currency]}`}/>
-        </Box>
+
+            <Badge
+              fontSize={"2xl"}
+              bgColor={"blackAlpha.800"}
+              color={"white"}
+            >{`#${coin.market_cap_rank}`}</Badge>
+
+            <CustomBar
+              high={`${currencySymbol}${coin.market_data.high_24h[currency]}`}
+              low={`${currencySymbol}${coin.market_data.low_24h[currency]}`}
+            />
+
+            <Box w={"full"} p="4">
+              <Item title={"Max Supply"} value={coin.market_data.max_supply} />
+              <Item
+                title={"Circulating Supply"}
+                value={coin.market_data.circulating_supply}
+              />
+              <Item
+                title={"Market Cap"}
+                value={`${currencySymbol}${coin.market_data.market_cap[currency]}`}
+              />
+              <Item
+                title={"All Time Low"}
+                value={`${currencySymbol}${coin.market_data.atl[currency]}`}
+              />
+              <Item
+                title={"All Time High"}
+                value={`${currencySymbol}${coin.market_data.ath[currency]}`}
+              />
+            </Box>
           </VStack>
         </>
-      )
-    }
-  </Container> 
+      )}
+    </Container>
+  );
 };
 
 const Item = ({ title, value }) => (
@@ -105,6 +207,7 @@ const Item = ({ title, value }) => (
     <Text>{value}</Text>
   </HStack>
 );
+
 const CustomBar = ({ high, low }) => (
   <VStack w={"full"}>
     <Progress value={50} colorScheme={"teal"} w={"full"} />
@@ -115,4 +218,5 @@ const CustomBar = ({ high, low }) => (
     </HStack>
   </VStack>
 );
+
 export default CoinDetails;
